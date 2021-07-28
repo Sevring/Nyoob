a68da86a0e46f8d0bbb8c7894b10078b0d6e1c5b
Sevring <88015374+Sevring@users.noreply.github.com>
2021-07-28 17:27:03 +0900

    210728
    C

--- Cmain.c
+++ Cmain.c
@@ -1,18 +1,194 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+
 
 int main()
 {
-	
-	char b[10] = "가나다라마";
-	int c[20] = {11,22,33};
-	printf("int size: %d\n", sizeof(int));
-	printf("float size : %d\n", sizeof(float));
-	printf("char size : %d\n", sizeof(char));
-	printf("char b size : %d\n", sizeof(b));
-	printf("int c[20] size : %d\n", sizeof(c));
+	//int input; 
+	//
+	//scanf("%d", &input);
+	//printf("%d\n", input);
+	//
+	//
+	//
+	//float f_input = 123.456;
+	//char c_input = 'a';
+	//scanf(" %f", &f_input);
+	//printf("입력된 인풋 : %f\n", f_input);
+	//scanf(" %c", &c_input);
+	//printf("입력된 인풋 : %c\n", c_input);
+	//------------------------------------------
 
-	for (int i = 0; i < 10; i++)
-	{
-		printf("char b[%d] = %c\n", i, b[i]);
-	}
+	//int a;
+	//float b;
+	//char c;
+	//scanf(" %d %f %c", &a, &b, &c);
+	//printf("int a : %d \nfloat b : %f \nchar c : %c", a, b, c);
+	//-----------------------------------------------------------
+
+	//int year;
+	//float height;
+	//char gender;
+	//printf("나이를 입력하세요 : ");
+	//scanf(" %d", &year);
+	//printf("입력된 나이 : %d\n", year);
+	//
+	//printf("키를 입력하세요 : ");
+	//scanf(" %f", &height);
+	//printf("입력된 키 : %f\n", height);
+	//
+	//printf("성별을 입력하세요(M,F) : ");
+	//scanf(" %c", &gender);
+	//printf("입력된 성별 : %c\n", gender);
+	//
+	//printf("자신의 나이, 키, 성별을 순서대로 입력하세요. : ");
+	//scanf(" %d %f %c", &year, &height, &gender);
+	//printf("입력된 나이 : %d\n입력된 키 : %f\n입력된 성별 : %c", year, height, gender);
+	//-------------------------------------------------------------------------------------
+
+	//float a, b; 
+	//printf("연산할 두 수를 띄워쓰기나 엔터로 구분하여 입력하세요.\n");
+	//scanf(" %f %f", &a, &b);
+	//printf("합 : %.2f\n차 : %.2f\n곱 : %.2f\n", a + b, a - b, a * b);
+	//printf("---------------------------------------------\n");
+	//int num;
+	//printf("출력할 구구단의 단 수를 입력하세요. : ");
+	//scanf(" %d", &num);
+	//printf("뀨단 %d단 : %d * 1 = %d\n", num, num, num*1);
+	//printf("뀨단 %d단 : %d * 2 = %d\n", num, num, num*2);
+	//printf("뀨단 %d단 : %d * 3 = %d\n", num, num, num*3);
+	//printf("뀨단 %d단 : %d * 4 = %d\n", num, num, num*4);
+	//printf("뀨단 %d단 : %d * 5 = %d\n", num, num, num*5);
+	//printf("뀨단 %d단 : %d * 6 = %d\n", num, num, num*6);
+	//printf("뀨단 %d단 : %d * 7 = %d\n", num, num, num*7);
+	//printf("뀨단 %d단 : %d * 8 = %d\n", num, num, num*8);
+	//printf("뀨단 %d단 : %d * 9 = %d\n", num, num, num*9);
+	//----------------------------------------------------------------------
+
+	//int a = 5; 
+	//printf("%d", 0% 3);
+
+	//int a = 5; 
+	//a++;
+	//printf("%d\n", a);
+	//++a;
+	//printf("%d\n", a);
+  //-----------------------
+
+	//int num;
+	//int m = 1;
+	//printf("출력할 구구단의 단 수를 입력하세요. : ");
+	//scanf(" %d", &num);
+	//printf("뀨단 %d단 : %d * 1 = %d\n", num, num,num* m++);
+	//printf("뀨단 %d단 : %d * 2 = %d\n", num, num,num* m++);
+	//printf("뀨단 %d단 : %d * 3 = %d\n", num, num,num* m++);
+	//printf("뀨단 %d단 : %d * 4 = %d\n", num, num,num* m++);
+	//printf("뀨단 %d단 : %d * 5 = %d\n", num, num,num* m++);
+	//printf("뀨단 %d단 : %d * 6 = %d\n", num, num,num* m++);
+	//printf("뀨단 %d단 : %d * 7 = %d\n", num, num,num* m++);
+	//printf("뀨단 %d단 : %d * 8 = %d\n", num, num,num* m++);
+	//printf("뀨단 %d단 : %d * 9 = %d\n", num, num,num* m++);
+	//
+	//int num2;
+	//int m2 = 1;
+	//printf("출력할 구구단의 단 수를 입력하세요. : ");
+	//scanf(" %d", &num2);
+	//printf("뀨단 %d단 : %d * 1 = %d\n", num2, num2, num2 * ++m2);
+	//printf("뀨단 %d단 : %d * 2 = %d\n", num2, num2, num2 * m2++);
+	//printf("뀨단 %d단 : %d * 3 = %d\n", num2, num2, num2 * m2++);
+	//printf("뀨단 %d단 : %d * 4 = %d\n", num2, num2, num2 * m2++);
+	//printf("뀨단 %d단 : %d * 5 = %d\n", num2, num2, num2 * ++m2);
+	//printf("뀨단 %d단 : %d * 6 = %d\n", num2, num2, num2 * m2++);
+	//printf("뀨단 %d단 : %d * 7 = %d\n", num2, num2, num2 * m2++);
+	//printf("뀨단 %d단 : %d * 8 = %d\n", num2, num2, num2 * m2++);
+	//printf("뀨단 %d단 : %d * 9 = %d\n", num2, num2, num2 * m2++);
+	//----------------------------------------------------------------
+
+	//int q1 = 10, q2 = 10;
+	//q1++;
+	//++q2;
+	//
+	//printf("q1 : %d\n", q1);
+	//printf("q2 : %d\n", q2);
+	//			   
+	//printf("q1 : %d\n", q1++);
+	//printf("q2 : %d\n", ++q2);
+	//			   			   
+	//printf("q1 : %d\n", q1);
+	//printf("q2 : %d\n", q2);
+	//-----------------------------------------------------
+
+	//printf("%d\n", 5 >= 10);
+	//printf("%d\n", 5 <= 10);
+	//
+	//printf("%d\n", 10 == 10);
+	//printf("%d\n", 10 != 10);
+
+	//int X = 0;
+	//printf("%d\n", -5 < X && X < 1);
+	//printf("%d\n", -5 < X < 1);
+// 예제
+// 숫자를 입력 받아서 0보다 크고 100보다 작거나 같은지 결과 값으로 출력하기
+
+	//float T;
+	//
+	//printf("판별한 수를 입력하세요. : ");
+	//scanf(" %f", &T);
+	//printf(" 판별값은 : %d 입니다.\n", 0 < T && T <= 100);
+//-------------------------------------------------------------
+
+	//printf("정수 출력 : %d\n", (int)3.14);
+	//printf("정수 출력 : %f\n", (float)3);
+	//int A = 10, B = 3;
+	//float C = (float)A / B;
+	//printf("%f\n\n\n", C);
+	//
+	//int k = 19;
+	//float j = 25.323;
+	//char z = "a";
+	//printf("%d %d %d %d %d", sizeof(k), sizeof(j), sizeof(z), sizeof(3.14), sizeof(3.14f));
+	//printf("%c", z);
+//-----------------------------------------------------------------
+//int t = 10;
+//printf("%d\n", t += 5);
+//printf("%d\n", t -= 5);
+//printf("%d\n", t *= 5);
+//printf("%d\n", t /= 5);
+//printf("%d\n", t %= 5);
+//-----------------------------------
+
+//float a, b;
+//printf("비교할 값을 입력하세요 : ");
+//scanf(" %f %f", &a, &b);
+//int result;
+//result = (a > b ? 25 : 50 );
+//printf("%d", result);
+//---------------------------------------
+//체중과 키를 입력해서 BMI를 구한후에 값이 20.0 이상 25.0미만이면 표준 이 외에는 체중관리로 출력
+
+float kg;
+float cm;
+float bmi;
+
+printf("체중을 입력하세요.(kg 단위) : ");
+scanf(" %f", &kg);
+printf("\n키를 입력하세요.(cm 단위) : ");
+scanf(" %f", &cm);
+cm *= 0.01f;
+
+bmi = kg / (cm * cm);
+
+//printf("체중이 %c", (bmi >= 20.0 && bmi < 25.0) ? out1 : out2);
+
+(bmi >= 20.0 && bmi < 25.0)
+	? printf("BMI수치는 : %.2fkg/(%.2fm*%.2fm) = %.2f \n표준체중입니다.",kg ,cm,cm,bmi)
+	: printf("BMI수치는 : %.2fkg/(%.2fm*%.2fm) = %.2f \n체중관리가 필요합니다.", kg, cm, cm, bmi);
+
+
+
+
+
+
+
+
 }
